dcc6b96689c6c39574b73d08a893ecb877cbed6d
MedElZy <65175021+MedElZy@users.noreply.github.com>
2021-01-03 17:51:27 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -3,7 +3,7 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m MEDEL
 sudo adduser MEDEL sudo
-echo 'ALOK:8426' | sudo chpasswd
+echo '123456' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -17,8 +17,8 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sudo adduser ALOK chrome-remote-desktop
+sudo apt install nautilus nano -y
+sudo adduser MEDEL chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
